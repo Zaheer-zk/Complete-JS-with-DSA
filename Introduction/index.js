6e47baa1383dd92fd04cdb1656056b31fcfd035f
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-06-24 17:18:46 +0530

    first commit

---
+++ Introduction/index.js
@@ -0,0 +1,62 @@
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello, JavaScript!!!";
+// }
+
+// Variables
+var a;
+a ='Javascript';
+var a = 'Javascript'
+
+// words vs keywords
+
+// My name is Zaheer -> word
+// for, forEach, map. -> keywords
+
+
+let age = 21;
+age = 21 + 1; // 22
+//We can reassign the value to the let variable
+//Scope -> Block level
+
+const birthYear = 2001; // The value that we define will not be changed
+// We can't change const declaration variable.
+// We can't declare const without initialization
+// We can use where you don't need to change the value of it.
+//Scope -> Block level
+
+var a = 10;
+//We can reassign the value to the var variable
+//Scope -> Function level
+
+// ## DATA TYPES IN JS
+// NUMBER, String, Boolean, Undefined, Null, Symbol, BigInt.
+
+let name = 'Zaheer'; // String
+let isCalled = false; // Boolean
+
+// ## JavaScript Operators
+// # Arithmetic Operators (+, -, *, /, %)
+// # Assignment operator ( = )
+// # Comparison operator 
+    // == Equality check
+    // != Inequality check
+    // === Identity check
+    // !== Non-identity check
+    // <
+    // >
+    //  <=
+    // >= 
+
+// # Logical Operators
+// && (AND)
+// || (OR) 
+// ! (NOT)
+// EX - 
+let a1 = 10;
+let b1 = 20;
+
+console.log(a1 == 10 && b1 == 30) // print result = true
+console.log(a1 == 10 || b1 == 30) // print result = true
+console.log(!(a1 == 10)) // print result = false
+
+
